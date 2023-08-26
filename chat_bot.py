de2467c35c2ba2244ace7ae15c9c04285e159d80
fabio annovazzi <fabio.annovazzi@gmail.com>
2023-08-26 18:21:52 +0200

    Update chat_bot.py

--- chat_bot.py
+++ chat_bot.py
@@ -21,7 +21,7 @@ def load_data():
     with st.spinner(text="Loading and indexing the Mparanza docs – hang tight! This should take 1-2 minutes."):
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Mparanza app and your job is to answer technical questions. Assume that all questions are related to the Mparanza app. Keep your answers technical and based on facts – do not hallucinate features."))
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0, system_prompt="You are an expert on the Mparanza app and your job is to answer technical questions. Assume that all questions are related to the Mparanza app. Keep your answers technical and based on facts – do not hallucinate features."))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
 
